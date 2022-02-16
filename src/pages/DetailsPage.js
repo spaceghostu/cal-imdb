c292030e3c52fbd5f222a18f615fe6083286a92a
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2022-02-16 17:13:48 +0200

    set up routes

---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function DetailsPage() {
+    return (
+        <>
+            <div>DetailsPage</div>
+            <Link to='/'>Home</Link>
+        </>
+    )
+}
