30d933243ec957aad66c5dc41d1972d7946e4eef
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2022-02-17 15:15:22 +0200

    ad debounce to search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,15 +4,20 @@ import React from 'react'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup';
 import { LinkContainer } from 'react-router-bootstrap';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 
 export default function Search() {
     const searchMovies = useStoreActions(actions => actions.movies.search);
     const setSearchQuery = useStoreActions(actions => actions.app.setSearchQuery);
     const searchQuery = useStoreState(actions => actions.app.searchQuery);
+    const setSearchQueryDebounced = AwesomeDebouncePromise(
+        setSearchQuery,
+        500,
+    );
     return (
         <>
             <InputGroup>
-                <Form.Control onChange={setSearchQuery} type="text" placeholder="Search..." />
+                <Form.Control onChange={setSearchQueryDebounced} type="text" placeholder="Search..." />
                 <LinkContainer to="/">
                     <Button onClick={() => searchMovies(searchQuery)}>Search</Button>
                 </LinkContainer>
