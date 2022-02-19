89427abf2fc1b5f60828581a40c3aab349008189
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2022-02-19 15:31:42 +0200

    resposiveness and some propper theming

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,41 +3,76 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Search from './Search';
-import { NavLink } from '../styled-components/NavLink';
 import { LinkContainer } from 'react-router-bootstrap';
 import Col from 'react-bootstrap/Col';
-import StyledNavbar from '../styled-components/Navbar';
 import Logo from './Logo';
+import styled from 'styled-components';
+import { Offcanvas } from 'react-bootstrap';
 
 export default function Header() {
-    return (
-        <StyledNavbar bg="dark" variant="dark">
-            <Container>
 
-                <Col md={{ span: 2 }}>
-                    <LinkContainer to="/">
-                        <Logo />
-                    </LinkContainer>
-                </Col>
-                <Col md={{ span: 6 }}>
+    const nav = (
+        <Nav className="justify-content-md-end">
+            <LinkContainer to="/">
+                <NavLink href="/favorites">
+                    Home
+                </NavLink>
+            </LinkContainer>
+            <LinkContainer to="/favorites">
+                <NavLink href="/favorites">
+                    Favorites
+                </NavLink>
+            </LinkContainer>
+        </Nav>
+    )
+    return (
+        <>
+            <StyledNavbar bg="dark" variant="dark">
+                <Container>
+                    <Col md={{ span: 2 }}>
+                        <LinkContainer to="/">
+                            <Logo />
+                        </LinkContainer>
+                    </Col>
+                    <Col md={{ span: 6 }} className="d-none d-sm-flex">
+                        <Search />
+                    </Col>
+                    <Col md={{ span: 2 }} className="d-none d-sm-flex">
+                        {nav}
+                    </Col>
+                    <Col className="d-xs-flex d-sm-none">
+                        <Navbar.Toggle className="d-flex ms-auto" />
+                        <Navbar.Offcanvas placement="end">
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                {nav}
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Col>
+                </Container>
+            </StyledNavbar>
+
+            <StyledNavbar bg="dark" variant="dark" className="d-xs-flex d-sm-none">
+                <Col xs={{ span: 10, offset: 1 }}>
                     <Search />
                 </Col>
-                <Col md={{ span: 2 }}>
-                    <Nav className="justify-content-md-end">
-                        <LinkContainer to="/">
-                            <NavLink href="/favorites">
-                                Home
-                            </NavLink>
-                        </LinkContainer>
-                        <LinkContainer to="/favorites">
-                            <NavLink href="/favorites">
-                                Favorites
-                            </NavLink>
-                        </LinkContainer>
-                    </Nav>
-                </Col>
-            </Container>
-        </StyledNavbar>
+            </StyledNavbar>
+        </>
     )
 }
 
+
+const StyledNavbar = styled(Navbar)`
+    background-color: #121212 !important;
+`
+
+const NavLink = styled(Nav.Link)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    svg {
+        margin-left: 8px;
+    }
+`
